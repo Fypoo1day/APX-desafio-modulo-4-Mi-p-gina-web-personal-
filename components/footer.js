c5c953919c6e380f18d5b2f5a23c13f178391bdc
Fypoolday <Fypoolday@users.noreply.github.com>
2022-02-21 18:30:25 -0300

    correccion links redes sociales

--- components/footer.js
+++ components/footer.js
@@ -9,13 +9,13 @@ function footerComponent(el) {
           </a>
         </div>
         <div class="footer__social">
-          <a href="https://www.instagram.com/_lucassruiz/"
+          <a href="https://www.instagram.com/alan_celasco/"
             >Instagram<img src="./assets/instagram.png" alt="Instagram"
           /></a>
-          <a href="https://www.linkedin.com/in/lucas-ruiz-32510621a/"
+          <a href="https://www.linkedin.com/in/alan-philipp-celasco-044232171/"
             >Linkedin<img src="./assets/linkedin.png" alt="Linkedin"
           /></a>
-          <a href="https://github.com/LksR-dev"
+          <a href="https://github.com/Fypoo1day"
             >Github<img src="./assets/github.png" alt="github"
           /></a>
       </div>
