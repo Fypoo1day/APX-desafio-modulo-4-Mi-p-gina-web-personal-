dfa29264b20b02de1140cdd19bddc481262948cf
Fypoolday <Fypoolday@users.noreply.github.com>
2022-02-21 18:16:13 -0300

    correccion header

--- components/header.js
+++ components/header.js
@@ -1,44 +1,37 @@
 function headerComponent(el) {
-  const componentEl = document.createElement("div");
-  componentEl.classList.add("header__nav");
+  const headerEl = document.createElement("div");
+  headerEl.classList.add("header__nav");
 
-  componentEl.innerHTML = `
-  
-  <a href="./index.html">
-    <h2 class="header__logo">ALAN</h2>
-  </a>
-  <nav>
-    <button class="hamburguer" id="hamburguer">
-      <i class="fas fa-bars"></i>
-    </button>
-    
-    <ul class="header__nav-links">
-      <li><a href="./portfolio.html">Portfolio</a></li>
-      <li><a href="./services.html">Servicios</a></li>
-      <li><a href="./contact.html">Contacto</a></li>
-    </nav>
-  </ul>
-  
+  headerEl.innerHTML = `
+    <a href="./index.html">
+      <h2 class="header__logo">ALAN</h2>
+    </a>
+      <button class="btn">
+          <span class="top-line"></span>
+          <span class="middle-line"></span>
+          <span class="botton-line"></span>
+        </button>
+      
+      <div class="header__window-nav">
+        <nav class="header__nav-links">
+          <a href="./portfolio.html">Portfolio</a
+          ><a href="./services.html">Servicios</a
+          ><a href="./contact.html">Contacto</a>
+        </nav>
+      </div>
   `;
 
-  el.appendChild(componentEl);
-  const burguerMenuEl = document.querySelector(".hamburguer");
-  const windowEl = document.querySelector(".header__nav-links");
+  el.appendChild(headerEl);
+  const btnEl = document.querySelector(".btn");
+  const windowEl = document.querySelector(".header__window-nav");
 
-  const hamburguer = document.getElementById("hamburguer");
-  const navUL = document.getElementById("hamburguer");
-
-  hamburguer.addEventListener("click", () => {
-    navUL.classList.toggle("show");
+  btnEl.addEventListener("click", () => {
+    if (btnEl.classList.contains("open")) {
+      btnEl.classList.remove("open");
+      windowEl.style.display = "";
+    } else {
+      btnEl.classList.add("open");
+      windowEl.style.display = "inherit";
+    }
   });
-
-  // burguerMenuEl.addEventListener("click", () => {
-  //   if (burguerMenuEl.classList.contains("open")) {
-  //     burguerMenuEl.classList.remove("open");
-  //     windowEl.style.display = "";
-  //   } else {
-  //     burguerMenuEl.classList.add("open");
-  //     windowEl.style.display = "inherit";
-  //   }
-  // });
 }
