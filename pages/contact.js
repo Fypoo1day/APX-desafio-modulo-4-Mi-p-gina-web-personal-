cbcdf9b2b307a5fb711db31af4c350625d8af99b
Fypoolday <Fypoolday@users.noreply.github.com>
2022-02-22 10:19:16 -0300

    página de servicios y contacto agregadas

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+function main() {
+  headerComponent(document.querySelector(".header__contact"));
+  contactComponent(document.querySelector(".form__contact-page"));
+  footerComponent(document.querySelector(".footer"));
+}
+
+main();
